c8aa9661a1c815ad9db488d8574b883f1efe65e4
Linus Norton <linusnorton@gmail.com>
2025-08-10 14:18:24 +0100

    added test

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./middleware/authenticate.js";
 
-import type { Express } from "express";
 import type { AuthenticatedRequest } from "./middleware/authenticate.js";
 
 export type { AuthenticatedRequest };
