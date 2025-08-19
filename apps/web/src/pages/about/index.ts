734d5133b8a6d57fe449104e0b8ba2861dbdffef
Linus Norton <linusnorton@gmail.com>
2025-08-19 20:35:08 +0100

    upadtes

--- apps/web/src/pages/about/index.ts
+++ apps/web/src/pages/about/index.ts
@@ -3,8 +3,14 @@ import type { Request, Response } from "express";
 export const GET = async (_req: Request, res: Response) => {
   // Use translations from res.locals (already populated by middleware)
   res.render("about/index", {
-    title: res.locals.pages?.about?.title || "About - HMCTS Monorepo",
-    description: res.locals.pages?.about?.description || "This page was automatically discovered by the file-system router",
+    en: {
+      title: "About - HMCTS Monorepo",
+      description: "This page was automatically discovered by the file-system router",
+    },
+    cy: {
+      title: "Amdanom - Templed Monorepo HMCTS",
+      description: "Dydy hon ddim yn dod o hyd i'r ffeil system",
+    },
   });
 };
 
