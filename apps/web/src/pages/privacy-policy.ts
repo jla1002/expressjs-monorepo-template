d153604b74b0cfc62f8b749ca603e347850a8a75
Linus Norton <linusnorton@gmail.com>
2025-08-20 10:24:22 +0100

    tidy up

--- apps/web/src/pages/privacy-policy.ts
+++ apps/web/src/pages/privacy-policy.ts
@@ -3,12 +3,10 @@ import type { Request, Response } from "express";
 export const GET = async (_req: Request, res: Response) => {
   res.render("index", {
     en: {
-      title: "HMCTS Monorepo Template",
-      description: "This is the home page of the Express Monorepo Service",
+      title: "Privacy Policy"
     },
     cy: {
-      title: "Templed Monorepo HMCTS",
-      description: "Dyma dudalen gartref y Gwasanaeth Monorepo Express",
+      title: "Polisi Preifatr"
     },
   });
 };
