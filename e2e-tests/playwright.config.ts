61c28d74080036c28ef2b2606815fac5f9d1fb23
Linus Norton <linusnorton@gmail.com>
2025-09-12 15:58:05 +0100

    force build before e2e tests

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'yarn dev',
+    command: 'yarn build:dev && yarn dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
